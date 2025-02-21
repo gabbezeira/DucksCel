c28f88ebfc89e900819583c8df1099ae3dc0544f
gabbezeira <gabrielalves.code@gmail.com>
2025-02-21 12:20:01 -0300

    ajustes

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -3,7 +3,7 @@ import { Container } from './styles'
 export const Footer = () => (
 	<Container>
 		<p className="copy">
-			2025 © Copyright - Todos os direitos reservados Ducks Cell
+			2025 © Copyright - Todos os direitos reservados DucksCel
 		</p>
 	</Container>
 )
